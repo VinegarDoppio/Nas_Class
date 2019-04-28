fef14ddc4a590dfc2b2f0d9c9c90ca53b448cd09
VinegarDoppio <50027770+VinegarDoppio@users.noreply.github.com>
2019-04-28 17:23:45 +0300

    Add files via upload

--- Pr.h
+++ Pr.h
@@ -1,23 +1,30 @@
 #ifdef _WIN32
 #include <tchar.h>
 #endif
-class Pr : public Pl {
+#include <math.h>
+class Pr {
 	private:
-	int z1,z2, V_V, S_pov;
+	int x1, x2, x3, y1, y2, y3, z1, z2, V_V, S_S;
 	public:
-	Pr (int x1, int x2, int x3, int y1, int y2, int y3, int z1,int z2) : Pl (x1, x2, x3, y1, y2, y3)
+	Pr (int x1, int x2, int x3, int y1, int y2, int y3, int z1,int z2)
 	{
+	    this->x1 = x1;
+		this->x2 = x2;
+		this->x3 = x3;
+		this->y1 = y1;
+		this->y2 = y2;
+		this->y3 = y3;
 		this->z1 = z1;
 		this->z2 = z2;
 	}
-	int V (S_S)
+	int V ()
 	{
-		V_V = S_S * (z2 - z1);
+		V_V = (x3 - x1) * (z2 - z1) * (y2 - y1);
 		return V_V;
 	}
-	int S_S2 (int S_S, int x1, int x3, int y2, int y1)
+	 float S ()
 	{
-	   S_pov = 2*S_S + 2*(z2 - z1) * (x3 - x1) + 2*(z2 - z1) * (y2 - y1);
-	   return S_pov;
+	   S_S = 2*(x3 - x1) * (y2 - y1) + 2*(z2 - z1) * (x3 - x1) + 2*(z2 - z1) * sqrt((pow(double(y2 - y1), 2)) + pow(double(x2 - x1), 2));
+	   return S_S;
 	}
 };
